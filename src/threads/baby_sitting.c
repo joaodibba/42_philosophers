b875cd0154941bd44a9c3e7c45320c90fa33eff1
joaodibba <joaomiranda.pessoal@gmail.com>
2024-01-05 19:36:27 +0000

    im killing myself

--- src/threads/baby_sitting.c
+++ src/threads/baby_sitting.c
@@ -6,7 +6,7 @@
 /*   By: jalves-c < jalves-c@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 17:22:38 by jalves-c          #+#    #+#             */
-/*   Updated: 2024/01/05 19:32:16 by jalves-c         ###   ########.fr       */
+/*   Updated: 2024/01/05 19:36:12 by jalves-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,41 +20,31 @@ void	check_if_baby_is_full(t_node *node)
 	pthread_mutex_unlock(&node->u_data.philo.mutex);
 }
 
-bool	is_all_full(void)
-{
-	t_node	*node;
-	static unsigned int full_philo = 0;
+bool is_all_full(void) {
+    t_node *node;
+    unsigned int full_philo = 0;
 
-	node = host()->head;
-	while (true)
-	{
-
-		if (node && node->type == FORK)
-			node = node->next;
-		if (node && node->type == PHILO)
-		{
-			pthread_mutex_lock(&node->u_data.philo.mutex);
-			if (node->u_data.philo.state == FULL)
-				full_philo++;
-			if(full_philo == host()->philo)
-				return (true);
-			pthread_mutex_unlock(&node->u_data.philo.mutex);
-			return (false);
-			pthread_mutex_unlock(&node->u_data.philo.mutex);
-			node = node->next;
-		}
-	}
-	return (true);
+    node = host()->head;
+    while (node) {
+        if (node->type == PHILO) {
+            pthread_mutex_lock(&node->u_data.philo.mutex);
+            if (node->u_data.philo.state == FULL) {
+                full_philo++;
+            }
+            pthread_mutex_unlock(&node->u_data.philo.mutex);
+        }
+        node = node->next;
+    }
+    return (full_philo == host()->philosopher_count);
 }
 
-void	baby_sitting(void)
-{
-	t_node *node;
-	bool	all_full = false;
 
-	node = host()->head;
-	if (host()->philosopher_count == 1)
-		return ;
-	while (all_full == false)
-		all_full = is_all_full();
+void baby_sitting(void) {
+    if (host()->philosopher_count == 1) {
+        return;
+    }
+
+    while (!is_all_full()) {
+        // Do nothing, just wait
+    }
 }
