8fa32dce9a5d775829cb0122c9d56fb0abd7bbd5
Joao Tomas Alves Costa Miranda <jalves-c@c2r5s1.42lisboa.com>
2024-01-05 20:21:32 +0000

    updt

--- src/threads/baby_sitting.c
+++ src/threads/baby_sitting.c
@@ -6,7 +6,7 @@
 /*   By: jalves-c < jalves-c@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 17:22:38 by jalves-c          #+#    #+#             */
-/*   Updated: 2024/01/05 19:36:12 by jalves-c         ###   ########.fr       */
+/*   Updated: 2024/01/05 19:41:37 by jalves-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,33 @@ void	check_if_baby_is_full(t_node *node)
 	pthread_mutex_unlock(&node->u_data.philo.mutex);
 }
 
-bool is_all_full(void) {
+bool is_all_full(void) 
+{
     t_node *node;
     unsigned int full_philo = 0;
 
     node = host()->head;
-    while (node) {
-        if (node->type == PHILO) {
+    while (node && full_philo != host()->philosopher_count) 
+	{
+        if (node->type == PHILO) 
+		{
             pthread_mutex_lock(&node->u_data.philo.mutex);
-            if (node->u_data.philo.state == FULL) {
+			if (node->u_data.philo.state == FULL)
                 full_philo++;
-            }
             pthread_mutex_unlock(&node->u_data.philo.mutex);
         }
         node = node->next;
+		printf("checking\n");
     }
-    return (full_philo == host()->philosopher_count);
+    return (true);
 }
 
 
-void baby_sitting(void) {
-    if (host()->philosopher_count == 1) {
-        return;
-    }
-
-    while (!is_all_full()) {
-        // Do nothing, just wait
-    }
+void baby_sitting(void) 
+{
+	bool	all_full = false;
+    if (host()->philosopher_count == 1)
+        return ;
+    while (!all_full)
+		all_full = is_all_full();
 }
