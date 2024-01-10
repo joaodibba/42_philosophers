655193fad0a7a418a94e2976d4f5e1a33fadf672
Joao Tomas Alves Costa Miranda <jalves-c@c1r19s12.42lisboa.com>
2024-01-10 19:03:07 +0000

    updt

--- src/threads/actions.c
+++ src/threads/actions.c
@@ -12,26 +12,42 @@
 
 #include "philo.h"
 
+void	ft_swap(pthread_mutex_t *a, pthread_mutex_t *b)
+{
+	pthread_mutex_t *tmp;
+
+	tmp = a;
+	*a = *b;
+	*b = *tmp;
+}
+
 void	devour(t_node	*node)
 {
-	pthread_mutex_lock(&node->prev->u_data.fork.mutex);
-	message(node->u_data.philo.id, YELLOW, "has taken a fork");
-	pthread_mutex_lock(&node->next->u_data.fork.mutex);
-	message(node->u_data.philo.id, YELLOW, "has taken a fork");
+	pthread_mutex_t *first;
+	pthread_mutex_t *second;
+
+	first = &node->prev->u_data.fork.mutex;
+	second = &node->prev->u_data.fork.mutex;
 	pthread_mutex_lock(&node->u_data.philo.mutex);
+	if (!(node->u_data.philo.id % 2))
+		ft_swap(first, second);
+	pthread_mutex_lock(first);
+	message(node->u_data.philo.id, YELLOW, "has taken a fork");
+	pthread_mutex_lock(second);
+	message(node->u_data.philo.id, YELLOW, "has taken a fork");
 	node->u_data.philo.last_meal = get_current_time();
 	node->u_data.philo.state = EAT;
 	node->u_data.philo.meal_count++;
 	message(node->u_data.philo.id, GREEN, "is eating");
 	ft_sleep(host()->time_to_eat);
+	pthread_mutex_unlock(second);
+	pthread_mutex_unlock(first);
 	pthread_mutex_unlock(&node->u_data.philo.mutex);
-	pthread_mutex_unlock(&node->next->u_data.fork.mutex);
-	pthread_mutex_unlock(&node->prev->u_data.fork.mutex);
 }
 
 void	nap(t_node	*node)
 {
-	pthread_mutex_lock(&(node->u_data.philo.mutex));
+	pthread_mutex_lock(&node->u_data.philo.mutex);
 	node->u_data.philo.state = SLEEP;
 	message(node->u_data.philo.id, BLUE, "is sleeping");
 	ft_sleep(host()->time_to_sleep);
